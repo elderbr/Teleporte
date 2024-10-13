dd3983048f9551964e0687bca6db25d329124ddc
ElderBR <elderbarbosaromao@gmail.com>
2024-10-13 17:52:03 -0300

    Refaturando o método que busca o mundo pelo nome, retorna world se existir

--- src/main/java/com/elderbr/mc/teleport/controllers/WorldsController.java
+++ src/main/java/com/elderbr/mc/teleport/controllers/WorldsController.java
@@ -1,7 +1,6 @@
 package com.elderbr.mc.teleport.controllers;
 
 import com.elderbr.mc.teleport.config.WorldConfig;
-import org.bukkit.Bukkit;
 import org.bukkit.World;
 
 import java.util.Objects;
@@ -14,7 +13,7 @@ public class WorldsController {
         if (Objects.isNull(name) || name.isBlank()) {
             throw new RuntimeException(String.format("Nome %s do mundo invalido!", name));
         }
-        World world = Bukkit.getWorld(worldConfig.getFindByName(name));
+        World world = worldConfig.findByName(name);
         if (Objects.isNull(world)) {
             throw new RuntimeException(String.format("Nome %s do mundo invalido!", name));
         }
