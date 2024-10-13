8eaeaeb7e30c5f6e66365238a5cb437d11a55c24
ElderBR <elderbarbosaromao@gmail.com>
2024-10-12 21:16:32 -0300

    Criado a classe para fazer CRUD do teleport

--- src/main/java/com/elderbr/mc/teleport/config/LocalConfig.java
+++ src/main/java/com/elderbr/mc/teleport/config/LocalConfig.java
@@ -1,23 +1,51 @@
 package com.elderbr.mc.teleport.config;
 
 import com.elderbr.mc.teleport.interfaces.Global;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
-public class LocationConfig implements Global {
+public class LocalConfig implements Global {
 
-    private static LocationConfig instance;
+    private static LocalConfig instance;
+    private File file = TELEPORT_FILE;
     private YamlConfiguration config;
 
-    private LocationConfig(){
-        config = FileConfig.getInstance().getConfig();
+    private LocalConfig(){
+        try {
+            if(!file.exists()){
+                file.createNewFile();
+            }
+        }catch (IOException e){
+            throw new RuntimeException("Erro ao criar o arquivo teleport.yml");
+        }
+        config = YamlConfiguration.loadConfiguration(file);
     }
 
-    public static LocationConfig getInstance(){
+    public static LocalConfig getInstance(){
         if(Objects.isNull(instance)){
-            instance = new LocationConfig();
+            instance = new LocalConfig();
         }
         return instance;
+    }
+
+    public void save(Player player, String name) {
+        Block local = player.getLocation().subtract(0, 1, 0).getBlock();
+        config.set(name + ".world", player.getWorld().getName());
+        config.set(name + ".location", local.getX()+" "+ local.getY()+" "+ local.getZ() );
+        save();
+    }
+
+    public boolean save(){
+        try{
+            config.save(file);
+            return true;
+        }catch (IOException e){
+            throw new RuntimeException("Erro ao salvar nova posição!");
+        }
     }
 }
