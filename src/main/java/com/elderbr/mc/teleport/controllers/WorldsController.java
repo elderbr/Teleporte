3cefb72d8f37139efe999a786116ad95050cf05b
ElderBR <elderbarbosaromao@gmail.com>
2024-10-13 17:22:12 -0300

    Criado a classe que controla o worlds e o método que buscar pelo nome

--- src/main/java/com/elderbr/mc/teleport/controllers/WorldsController.java
+++ src/main/java/com/elderbr/mc/teleport/controllers/WorldsController.java
@@ -1,2 +1,17 @@
-package com.elderbr.mc.teleport.controllers;public class WorldsController {
+package com.elderbr.mc.teleport.controllers;
+
+import com.elderbr.mc.teleport.config.WorldConfig;
+
+import java.util.Objects;
+
+public class WorldsController {
+
+    private WorldConfig worldConfig = WorldConfig.getInstance();
+
+    public String getFindByName(String world){
+        if(Objects.isNull(world) || world.isBlank()){
+            throw new RuntimeException(String.format("Nome %s do mundo invalido!", world));
+        }
+        return worldConfig.getFindByName(world);
+    }
 }
