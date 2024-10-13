dba01fd1228ec4ccf268bbe4405c759d8c204776
ElderBR <elderbarbosaromao@gmail.com>
2024-10-13 20:28:31 -0300

    Refaturando a criação de um novo mundo

--- src/main/java/com/elderbr/mc/teleport/controllers/WorldsController.java
+++ src/main/java/com/elderbr/mc/teleport/controllers/WorldsController.java
@@ -2,6 +2,7 @@ package com.elderbr.mc.teleport.controllers;
 
 import com.elderbr.mc.teleport.config.WorldConfig;
 import com.elderbr.mc.teleport.enums.WorldType;
+import com.elderbr.mc.teleport.util.Msg;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
@@ -17,33 +18,37 @@ public class WorldsController {
 
     public boolean create(Player player, String worldName){
         if(Objects.isNull(player) || !player.isOp()){
-            throw new RuntimeException("Ops, você não tem permissão para usar esse comando!");
+            throw new RuntimeException("§l§cOps, você não tem permissão para usar esse comando!");
         }
         if(Objects.isNull(worldName) || worldName.isBlank()){
-            throw new RuntimeException("Nome do mundo invalido!");
+            throw new RuntimeException("§l§cNome do mundo invalido!");
         }
+        Msg.PlayerAll(String.format("§l§eCriando novo mundo %s!", worldName));
         worldConfig.createWorld("world_"+worldName); // Salva novo mundo
+        Msg.PlayerAll(String.format("§l§eMundo %s criado com sucesso!", worldName));
         return true;
     }
 
     public boolean create(Player player, String worldName, WorldType type) {
         if(Objects.isNull(player) || !player.isOp()){
-            throw new RuntimeException("Ops, você não tem permissão para usar esse comando!");
+            throw new RuntimeException("§l§cOps, você não tem permissão para usar esse comando!");
         }
         if(Objects.isNull(worldName) || worldName.isBlank()){
-            throw new RuntimeException("Nome do mundo invalido!");
+            throw new RuntimeException("§l§cNome do mundo invalido!");
         }
+        Msg.PlayerAll(String.format("§l§eCriando novo mundo %s!", worldName));
         worldConfig.createWorld("world_"+worldName, type); // Salva novo mundo escolhendo o tipo
+        Msg.PlayerAll(String.format("§l§eMundo %s criado com sucesso!", worldName));
         return true;
     }
 
     public World findByName(String name) {
         if (Objects.isNull(name) || name.isBlank()) {
-            throw new RuntimeException(String.format("Nome %s do mundo invalido!", name));
+            throw new RuntimeException(String.format("§l§cNome %s do mundo invalido!", name));
         }
         World world = worldConfig.findByName(name);
         if (Objects.isNull(world)) {
-            throw new RuntimeException(String.format("Nome %s do mundo invalido!", name));
+            throw new RuntimeException(String.format("§l§cNome %s do mundo invalido!", name));
         }
         return world;
     }
