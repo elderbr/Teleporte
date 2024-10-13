a61455fd10f9e32fa14fe23ee1d8b3c93066b940
ElderBR <elderbarbosaromao@gmail.com>
2024-10-12 21:40:54 -0300

    Refaturado o método que lista os nomes dos locais

--- src/main/java/com/elderbr/mc/teleport/comandos/CommandTab.java
+++ src/main/java/com/elderbr/mc/teleport/comandos/CommandTab.java
@@ -1,7 +1,7 @@
 package com.elderbr.mc.teleport.comandos;
 
 
-import com.elderbr.mc.teleport.config.FileConfig;
+import com.elderbr.mc.teleport.interfaces.Global;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -10,34 +10,28 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommandTab implements TabCompleter {
+public class CommandTab implements TabCompleter, Global {
 
     private Player player;
-    private FileConfig fileConfig;
 
-    private List<String> locationList;
-
-    private String arg;
+    private List<String> names;
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 
+        names = new ArrayList<>();
+
         if (sender instanceof Player) {
-            fileConfig = FileConfig.getInstance();
-            locationList = new ArrayList<>();
             player = ((Player) sender).getPlayer();
-
-            arg = "";
-
-            for(String x : args){
-                arg += x+" ";
+            if (command.getName().equalsIgnoreCase("tpa")) {
+                for (String value : LOCATION_LIST) {
+                    if (value.contains(args[0])) {
+                        names.add(value);
+                    }
+                }
+                return names;
             }
-
-            if( args.length > 0){
-                return  fileConfig.list(arg.trim());
-            }
-
         }
-        return null;
+        return names;
     }
 }
