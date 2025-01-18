b6050a1a6da45144a749114bb40e2445768466bc
ElderBR <elderbarbosaromao@gmail.com>
2025-01-18 09:09:04 -0300

    Pom: Atualizado a versão do Spigot e a versão do plugin

---
+++ src/main/java/com/elderbr/mc/teleport/comandos/WorldTab.java
@@ -0,0 +1,54 @@
+package com.elderbr.mc.teleport.comandos;
+
+import com.elderbr.mc.teleport.controllers.TeleportController;
+import com.elderbr.mc.teleport.controllers.WorldsController;
+import com.elderbr.mc.teleport.enums.WorldType;
+import com.elderbr.mc.teleport.util.Text;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class WorldCommand implements CommandExecutor {
+
+    private String name;
+    private WorldsController worldCtrl = new WorldsController();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            name = Text.toString(args);
+
+            switch (command.getName().toLowerCase()) {
+                case "createworld":
+                    if (args.length > 1) {
+                        return worldCreateType(player, args);
+                    }
+                    return worldCreate(player);
+                case "deleteworld":
+                    return worldCtrl.delete(player, name);
+            }
+        }
+        return false;
+    }
+
+    private boolean worldCreate(Player player) {
+        return worldCtrl.create(player, name);
+    }
+
+    private boolean worldCreateType(Player player, String[] args) {
+        name = args[0];
+        String type = "normal";
+        WorldType worldType = WorldType.NORMAL;
+        if (args.length > 1) {
+            type = args[1];
+            if (type.equals("nether")) {
+                worldType = WorldType.NETHER;
+            } else if (type.equals("the_end")) {
+                worldType = WorldType.THE_END;
+            }
+        }
+        return worldCtrl.create(player, name, worldType);
+    }
+}
